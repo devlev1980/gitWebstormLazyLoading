62d5080e87a2c1aef57df2f7fec8fa3289b4f970
yevgenyl <yevgenyl@consist.co.il>
2018-07-29 11:25:53 +0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule'
+  },
+  {
+    path: 'orders', loadChildren: 'app/orders/orders.module#OrdersModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
