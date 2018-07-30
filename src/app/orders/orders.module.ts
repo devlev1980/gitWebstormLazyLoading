7e793643beffb77eed0bb1ef7698b95df1f53e2b
Yevgeny Levin <devlev1980@gmail.com>
2018-07-30 22:03:45 +0300

    adding table with images + columns + paginator

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import {OrdersListComponent} from './orders-list.component';
 import {LastFmService} from '../services/last-fm.service';
+import {SharedModule} from '../shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    SharedModule.forRoot()
   ],
   providers: [LastFmService],
   declarations: [OrdersListComponent]
